30c2b9d42d79da4cea76d073a774ea9a8d15d3d0
Akalo Mekonen <mekonen168@gmail.com>
2020-01-22 20:06:42 +0200

    widget title elemnt was set

--- src/component/Options.js
+++ src/component/Options.js
@@ -3,10 +3,11 @@ import Insert from './Insert'
 class Options extends React.Component{
     render(){
       const newDelete = this.props.deleteInsert
+      const optionTitle = "Please add an option"
       return(
         <div>
         <div className="widget-header">
-        <h3>Your Options</h3>
+        <h3 className="widget-header__title">Your Options</h3>
         <button className="button button--link"
             onClick = {this.props.deleteItem}
                 disabled = {this.props.options==0}>Remove All</button>
@@ -14,6 +15,7 @@ class Options extends React.Component{
              <Insert key={insert} insertValue = {insert} newDelete={newDelete} />
              )}
           </div>
+          <p>{optionTitle}</p>
           </div>
       )
     }
