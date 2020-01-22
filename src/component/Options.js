6a9fc61c5952efcd8a9089b6eba7d56c01322262
Akalo Mekonen <mekonen168@gmail.com>
2020-01-22 08:53:54 +0200

    the background of widgiet in Options has styled

--- src/component/Options.js
+++ src/component/Options.js
@@ -5,12 +5,15 @@ class Options extends React.Component{
       const newDelete = this.props.deleteInsert
       return(
         <div>
+        <div className="widget-header">
+        <h3>Your Options</h3>
         <button className="button button--link"
             onClick = {this.props.deleteItem}
                 disabled = {this.props.options==0}>Remove All</button>
              {this.props.options.map((insert)=>
              <Insert key={insert} insertValue = {insert} newDelete={newDelete} />
              )}
+          </div>
           </div>
       )
     }
