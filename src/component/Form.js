5fdb7afc6fe3cbce980ff7e1d76ed1db6ba36055
Akalo Mekonen <mekonen168@gmail.com>
2020-01-30 20:12:01 +0200

    I have fixed the case of duplicated item and no item added

--- src/component/Form.js
+++ src/component/Form.js
@@ -3,18 +3,27 @@ class Form extends React.Component{
   constructor(){
     super();
     this.handleFormInsert = this.handleFormInsert.bind(this)
+    this.state = {
+      error:undefined
+    }
   }
   handleFormInsert(e){
       e.preventDefault()
       const insert = e.target.elements.insert.value
+      const error = this.props.handleFormInsert(insert)
       if(insert){
-       this.props.handleFormInsert(insert)     
+       this.props.handleFormInsert    
       }
+      this.setState(()=>({error}))
+      if(!error){
+        e.target.elements.insert.value = ''
+       }
      }
+     
 render(){   
     return( 
         <div>
-        
+           {this.state.error &&<p>{this.state.error}</p>}
           <form onSubmit = {this.handleFormInsert}>
              <input type = "text" name = "insert"/>
              <button className = "button">Add Option</button>
